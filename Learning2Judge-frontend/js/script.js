b8fe26e5bf460dc5039ecf50978b02cea40ae6b5
Cvangen91 <263343@usn.no>
2025-03-04 11:55:21 +0100

    Did some prettier formatting and added a folder

--- Learning2Judge-frontend/js/script.js
+++ Learning2Judge-frontend/js/script.js
@@ -1,11 +1,12 @@
-
 /*Javascript for hamburgermenyen */
-document.addEventListener("DOMContentLoaded", function() {
-    /*venter til dokumentet kjører før scriptet kjører */
-    const menuToggle = document.querySelector(".menu-toggle"); /*henter hemburgermeny knappen */
-    const navLinks = document.querySelector(".nav-links"); /*henter navigasjonsmenyen */
-/*legger til en klikkhendelse på hamburgermenyen */
-    menuToggle.addEventListener("click", function() {
-        navLinks.classList.toggle("active"); //veksle mellom å vise/skjule menyen
-    });
+document.addEventListener('DOMContentLoaded', function () {
+  /*venter til dokumentet kjører før scriptet kjører */
+  const menuToggle =
+    document.querySelector('.menu-toggle'); /*henter hemburgermeny knappen */
+  const navLinks =
+    document.querySelector('.nav-links'); /*henter navigasjonsmenyen */
+  /*legger til en klikkhendelse på hamburgermenyen */
+  menuToggle.addEventListener('click', function () {
+    navLinks.classList.toggle('active'); //veksle mellom å vise/skjule menyen
+  });
 });
